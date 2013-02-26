9dd9db8ae6bab463f26ad37724dd01dde2829a01
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2013-02-26 22:35:01 +0100

    test naïve bayes

--- src/ml_in_action/ch04.clj
+++ src/ml_in_action/ch04.clj
@@ -13,8 +13,46 @@
     ["mr", "licks", "ate", "my", "steak", "how", "to", "stop","him"] :normal
     ["quit", "buying", "worthless", "dog", "food", "stupid"] :abusive}))
 
+;;TODO improve tokenizer to accept words with unicode
+;;TODO use stopwords
+(defn tokenize [^String text]
+  (keep #(when (> (.length %) 2) (.toLowerCase %))
+        (clojure.string/split text #"\W+")))
+
+(defn email-tokenize
+  "tokenizes text from raw email.
+The email text is everything after the first blank line."
+  [^String text]
+  (mapcat tokenize
+          (next (drop-while #(not (.isEmpty (.trim %)))
+                            (clojure.string/split-lines text)))))
+
+(defn assoc-map [k f coll] (map (fn [m] (assoc m k (f m))) coll))
+;; (c/assoc-map :feature (comp c/tokenize :raw) d)
+
+(defn load-data
+  "Load data from files and apply the tokenizer to extract features
+"
+  [tokenizer & class-files]
+  (mapcat
+   (fn [[clas dir]]
+     (let [dir (clojure.java.io/file dir)
+           files (if (.isFile dir) [dir]
+                     (map (partial clojure.java.io/file dir) (.list dir)))]
+       (map (fn [f] (let [raw (slurp (.getAbsolutePath f))]
+                      {:class (keyword clas)
+                       :raw raw
+                       :feature (tokenizer raw)
+                       }))
+            files)))
+   (partition 2 class-files)))
+
+(defn sample-set
+  "Splits the sample set into 2 groups train, test "
+  [coll]
+  (group-by (fn [x] ({0 :train 1 :test} (rand-int 2))) coll))
+
 ;;
-;; training set each has a class , a list of features
 ;;
 
 (defn counting
@@ -71,6 +109,26 @@ prior : proportion of items for this class.
                           (map #(get % clas) counts-by-class))])
                       classes))))))
 
+(defn check-model
+  "Checks model against a list of test condidates "
+  [model test]
+  (let [model (comp last model) ;; because model returns sorted classes
+        ]
+    (map (fn [m]
+           (merge m (zipmap [:guess :prob] (model (:feature m)))))
+         test)))
+
+(defn stats
+  "Returns total and a map of failed counts {guess => correct => count}"
+  [res]
+  (reduce (fn [stats {:keys [guess class]}]
+            (as-> stats x
+                (update-in x [:total] inc)
+                (if (= guess class) x
+                    (update-in x [:failed guess class] (fnil inc 0)))))
+          {:total 0 :failed {}} res))
+
+
 (comment
   (def b (c/bayes-inference c/posting-list))
 (def lb (c/bayes-inference c/posting-list c/log-bayes-rule))
@@ -78,4 +136,21 @@ user> (b ["maybe", "not", "take", "him", "to", "dog", "park", "stupid"])
 ([:normal 7.233796296296299E-15] [:abusive 1.766413792416646E-10])
 user> (lb ["maybe", "not", "take", "him", "to", "dog", "park", "stupid"])
 ([:normal -24.2730128349438] [:abusive -18.405536949199654])
-  )
+user> (c/stats (c/check-model lb s))
+{:count 27, :failed {:spam {:ham 5}}}
+
+user> (def d2 (c/load-data c/email-tokenize :ham  ".../ML_for_Hackers/03-Classification/data/easy_ham_2" :spam ".../ML_for_Hackers/03-Classification/data/spam_2"))
+#'user/d2
+user> (count d2)
+2799
+user> (def lb (c/bayes-inference d :rule c/log-bayes-rule))
+#'user/lb
+user> (def b (c/bayes-inference d ))
+#'user/b
+user> (c/stats (c/check-model lb d2))
+{:total 2799, :failed {:ham {:spam 297}, :spam {:ham 8}}}
+user> (c/stats (c/check-model b d2))
+{:total 2799, :failed {:ham {:spam 1353}}}
+
+
+)
